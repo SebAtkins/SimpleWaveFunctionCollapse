8bee1a7579bb4d604292aba3ccc65382aa71273d
SebAtkins <sebatkins@outlook.com>
2022-04-22 20:54:47 +0100

    Swagger

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageTk
+import tkinter as tk
 from random import randint
 
 if __name__ == "__main__":
@@ -7,11 +8,18 @@ if __name__ == "__main__":
 	#0,0,100 - Blue
 	colours = [(0,100,0), (0,0,100)]
 
+	root = tk.Tk()
+	canvas = tk.Canvas(root, height = 200, width = 200)
+
 	img = Image.new("RGB", (128, 128))
 	draw = ImageDraw.Draw(img)
 
 	for x in range(64):
 		for y in range(64):
 			draw.rectangle([4*x,4*y,4*(x+1),4*(y+1)], fill = colours[randint(0,1)])
+	
+	image = ImageTk.PhotoImage(img)
+	imagesprite = canvas.create_image(100, 100, image=image)
+	canvas.pack()
 
-	img.show()
+	root.mainloop()
