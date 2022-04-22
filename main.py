339e40eafdfe7e17193d7a8807e61fc15e68eb9a
SebAtkins <sebatkins@outlook.com>
2022-04-22 20:45:14 +0100

    Swag

---
+++ main.py
@@ -0,0 +1,19 @@
+from PIL import Image, ImageDraw
+from random import randint
+
+if __name__ == "__main__":
+	#Contains RGB values fo valid colours
+	#0,100,0 - Green
+	#0,0,100 - Blue
+	colours = [(0,100,0), (0,0,100)]
+
+	img = Image.new("RGB", (128, 128))
+	draw = ImageDraw.Draw(img)
+
+	
+
+	for x in range(64):
+		for y in range(64):
+			draw.rectangle([4*x,4*y,4*(x+1),4*(y+1)], fill = colours[randint(0,1)])
+
+	img.show()
